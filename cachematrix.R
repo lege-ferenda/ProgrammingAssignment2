0fb99191983f32323cf846bec06e2e176646df20
lege-ferenda <dimitris.a.fistiklis@gmail.com>
2014-08-16 23:10:01 +0300

    prog.assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function calculates the inverse of a square matrix and 
+##stores its value in the cache memory
 
 makeCacheMatrix <- function(x = matrix()) {
-
+		m <- NULL
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
 }
+		smatrix <- function(solve) m <<- solve
+		gmatrix <- function() m
+		list(set = set, get = get,
+		smatrix = smatrix, gmatrix = gmatrix)
+		}
+	
 
-
-## Write a short comment describing this function
+## cache if.. hope it works
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		m <- x$gmatrix
+		if(!is.null(m) {
+		print("getting cached data")
+		return(m)
+}
+		data <- x$get()
+		m <- solve(data, ...)
+		x$smatrix(m)
+		m
+
 }
